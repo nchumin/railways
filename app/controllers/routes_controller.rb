8a4856161806d32b92483d7c8ae70910180af1ea
Nikolay Ch <chumin.n@gmail.com>
2017-04-21 22:25:55 +0300

    Lesson 12 - add route resource

---
+++ app/controllers/routes_controller.rb
@@ -0,0 +1,50 @@
+class RoutesController < ApplicationController
+  def index
+    @routes = Route.all
+  end
+
+  def show
+    @route = Route.find(params[:id])
+  end
+
+  def new
+    @route = Route.new
+  end
+
+  def edit
+    @route = Route.find(params[:id])
+  end
+
+  def create
+    @route = Route.new(route_params)
+
+    if @route.save
+      redirect_to @route
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @route = Route.find(params[:id])
+
+    if @route.update(route_params)
+      redirect_to @route
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @route = Route.find(params[:id])
+    @route.destroy
+
+    redirect_to routes_path
+  end
+
+  private
+
+  def route_params
+    params.require(:route).permit(:name)
+  end
+end
---
+++ app/models/route.rb
@@ -0,0 +1,4 @@
+class Route < ApplicationRecord
+  validates :name, presence: true,
+                   length: { minimum: 5}
+end
