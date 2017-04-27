107fadec01c9c4ed4157aacf5b3c6debef05510a
Nikolay Ch <chumin.n@gmail.com>
2017-04-27 13:07:15 +0300

    Add references with tickets - train and tickets - user

--- db/migrate/20170427092540_create_tickets.rb
+++ db/migrate/20170427092540_create_tickets.rb
@@ -1,6 +1,11 @@
 class CreateTickets < ActiveRecord::Migration[5.0]
   def change
     create_table :tickets do |t|
+      t.string :number
+      t.belongs_to :user, index: true
+      t.belongs_to :train, index: true
+      t.belongs_to :railway_station, :start_station, index: true
+      t.belongs_to :railway_station, :end_station, index: true
 
       t.timestamps
     end
