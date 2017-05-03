23dbe3fefae64f03612209b582323944d389c2b4
Nikolay Ch <chumin.n@gmail.com>
2017-05-03 22:35:10 +0300

    Add validation to Trains

--- app/models/train.rb
+++ app/models/train.rb
@@ -2,6 +2,6 @@ class Train < ApplicationRecord
   belongs_to :route
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
   has_many :tickets
-  
+
   validates :number, presence: true
 end
