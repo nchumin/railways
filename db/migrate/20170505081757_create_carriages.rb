3ad563665d1e7ed138db035295e720113b8b5992
Nikolay Ch <chumin.n@gmail.com>
2017-05-05 12:07:23 +0300

    Add create carriages migration

--- db/migrate/20170505081757_create_carriages.rb
+++ db/migrate/20170505081757_create_carriages.rb
@@ -1,6 +1,11 @@
 class CreateCarriages < ActiveRecord::Migration[5.0]
   def change
     create_table :carriages do |t|
+      t.integer "number"
+      t.integer "top_seats"
+      t.integer "bottom_seats"
+      t.integer "train_id"
+      t.string "c_type"
 
       t.timestamps
     end
