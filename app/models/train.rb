f90ea4945a5ff638d586769066eae617bb2e44dd
Nikolay Ch <chumin.n@gmail.com>
2017-05-07 12:38:53 +0300

    move train logic to model

--- app/models/train.rb
+++ app/models/train.rb
@@ -5,4 +5,16 @@ class Train < ApplicationRecord
   has_many :carriages
 
   validates :number, presence: true
+
+  def count_carriages(c_type)
+    self.carriages.where(c_type: c_type).count
+  end
+
+  def count_top_seats(c_type)
+    self.carriages.where(c_type: c_type).sum('top_seats')
+  end
+
+  def count_bottom_seats(c_type)
+    self.carriages.where(c_type: c_type).sum('bottom_seats')
+  end
 end
