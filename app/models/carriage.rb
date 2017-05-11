4436730fbf399fed9ed33bde5ee70367d5be3737
Nikolay Ch <chumin.n@gmail.com>
2017-05-11 22:46:41 +0300

    Add scopes and set_number method

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -2,4 +2,23 @@ class Carriage < ApplicationRecord
   belongs_to :train
 
   validates :number, presence: true
+  validates :number, uniqueness: { scope: :train_id }
+
+  before_validation :set_number
+
+  scope :economy, -> { where(type: 'EconomyCarriage') }
+  scope :coupe, -> { where(type: 'CoupeCarriage') }
+  scope :luxe, -> { where(type: 'LuxeCarriage') }
+  scope :sitting, -> { where(type: 'SittingCarriage') }
+  scope :ordered, -> { order(:number) }
+
+  private
+
+  def set_number
+    self.number = if train.carriages.count.zero?
+                    1
+                  else
+                    train.carriages.last.number + 1
+                  end
+  end
 end
