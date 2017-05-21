5ba7f486c19470b4811d11369a4c65ab4b718767
Nikolay Chumin <chumin.n@gmail.com>
2017-05-21 20:11:07 +0300

    add specifig redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    if current_user.admin?
+      welcome_index_path
+    else
+      search_path
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
