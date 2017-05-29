aac0c38aa19a2555f52cffdb6d6071c94da96345
Nikolay Chumin <chumin.n@gmail.com>
2017-05-29 13:17:41 +0300

    add search.js

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,14 @@
+$(document).on('turbolinks:load', function() {
+    $('.search-form').submit(function() {
+        var start_station;
+        var end_station;
+
+        start_station = $(this).find('#start_station');
+        end_station = $(this).find('#end_station');
+
+        if (start_station.val() == end_station.val()) {
+            alert('Выберите конечную станцию');
+            return false;
+        }
+    });
+})
